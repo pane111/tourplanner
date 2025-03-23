0818e4f2eff4f5a17afeb7c7b61ceb5eeca14b81
pane111 <jakob.njh@gmail.com>
2025-03-23 21:03:25 +0100

    Updated ui

--- src/main/java/org/example/tourplanner/HelloApplication.java
+++ src/main/java/org/example/tourplanner/HelloApplication.java
@@ -2,22 +2,29 @@ package org.example.tourplanner;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+
+        // Load the CSS file
+        scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
+        primaryStage.setMinWidth(800);
+        primaryStage.setMinHeight(450);
+        primaryStage.setTitle("Tour Planner");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
