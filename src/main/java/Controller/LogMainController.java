47298628b00eab77b3a8dd7e9d8a2d27f8fb355b
Laurin Sonner <laurin.sonner01@gmail.com>
2025-03-30 21:29:12 +0200

    Add Log Button

--- src/main/java/Controller/LogMainController.java
+++ src/main/java/Controller/LogMainController.java
@@ -1,4 +1,31 @@
 package Controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LogMainController {
+
+    @FXML
+    private void onAddLog() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/AddLogView.fxml"));
+            Parent root = fxmlLoader.load();
+
+            Stage stage = new Stage();
+            stage.setTitle("Add New Log");
+            stage.getIcons().add(new Image(getClass().getResourceAsStream("/images/icon.png")));
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
