67d65be87e960c62028627b22c7e2ac3f0c58e28
pane111 <jakob.njh@gmail.com>
2025-03-30 21:48:29 +0200

    Merged conflicts

--- src/main/java/Controller/LogMainController.java
+++ src/main/java/Controller/LogMainController.java
@@ -4,9 +4,15 @@ import Model.TourLog;
 import ViewModel.LogListViewModel;
 import ViewModel.LogMainViewModel;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 import javax.print.attribute.standard.Media;
+import java.io.IOException;
 
 public class LogMainController {
 
@@ -42,5 +48,21 @@ public class LogMainController {
     public void setLog(TourLog log)
     {
         viewModel.setCurLog(log);
+    }
+
+    @FXML
+    private void onAddLog() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/AddLogView.fxml"));
+            Parent root = fxmlLoader.load();
+
+            Stage stage = new Stage();
+            stage.setTitle("Add New Log");
+            stage.getIcons().add(new Image(getClass().getResourceAsStream("/images/icon.png")));
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
