179cd168aa8818d7fd6d253174e4f4e3cfcde0a1
pane111 <jakob.njh@gmail.com>
2025-05-11 20:43:28 +0200

    Exception handling

--- src/main/java/bl/TourService.java
+++ src/main/java/bl/TourService.java
@@ -2,6 +2,8 @@ package bl;
 
 
 import Model.TourDto;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -12,19 +14,37 @@ import java.util.List;
 public class TourService {
     private final RestTemplate restTemplate = new RestTemplate();
     private final String API_URL = "http://localhost:8080/tours";
-
+    Logger logger = LogManager.getLogger(TourService.class);
     public TourDto[] fetchTours() {
-        ResponseEntity<TourDto[]> response = restTemplate.getForEntity(API_URL, TourDto[].class);
-        return response.getBody();
+        try {
+            ResponseEntity<TourDto[]> response = restTemplate.getForEntity(API_URL, TourDto[].class);
+            return response.getBody();
+        }
+        catch (Exception e) {
+            logger.error(e);
+        }
+        return new TourDto[0];
     }
     public TourDto createTour(TourDto tour) {
-        tour.setId(null);
-        return restTemplate.postForObject(API_URL, tour, TourDto.class);
+        try {
+            tour.setId(null);
+            return restTemplate.postForObject(API_URL, tour, TourDto.class);
+        }
+        catch (Exception e) {
+            logger.error(e);
+        }
+        return null;
     }
     public TourDto updateTour(TourDto updatedTour) {
-        String url = API_URL + "/" + updatedTour.getId();  // Construct the URL with the specific ID
-        restTemplate.put(url, updatedTour);  // Perform the PUT request
-        return updatedTour;  // Return the updated tour (or you could fetch it again after the update if needed)
+        try {
+            String url = API_URL + "/" + updatedTour.getId();
+            restTemplate.put(url, updatedTour);
+            return updatedTour;
+        }
+        catch (Exception e) {
+            logger.error(e);
+        }
+        return null;
     }
 
 
